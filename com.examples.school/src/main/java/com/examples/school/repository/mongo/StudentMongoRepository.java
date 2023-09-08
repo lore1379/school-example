cceb1cbcfaa10845527c7426036d3d912882392c
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-08 09:54:36 +0200

    Implemented save

--- com.examples.school/src/main/java/com/examples/school/repository/mongo/StudentMongoRepository.java
+++ com.examples.school/src/main/java/com/examples/school/repository/mongo/StudentMongoRepository.java
@@ -43,8 +43,10 @@ public class StudentMongoRepository implements StudentRepository {
 
 	@Override
 	public void save(Student student) {
-		// TODO Auto-generated method stub
-
+		studentCollection.insertOne(
+				new Document()
+					.append("id", student.getId())
+					.append("name", student.getName()));
 	}
 
 	@Override
