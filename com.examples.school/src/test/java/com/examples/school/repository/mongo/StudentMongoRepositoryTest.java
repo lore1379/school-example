28bdd1022140a62ef2044ff287efaa0d414e7060
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-08 09:22:56 +0200

    Test findById found

--- com.examples.school/src/test/java/com/examples/school/repository/mongo/StudentMongoRepositoryTest.java
+++ com.examples.school/src/test/java/com/examples/school/repository/mongo/StudentMongoRepositoryTest.java
@@ -77,6 +77,14 @@ public class StudentMongoRepositoryTest {
 		assertThat(studentRepository.findById("1"))
 			.isNull();
 	}
+	
+	@Test
+	public void testFindByIdFound() {
+		addTestStudentToDatabase("1", "test1");
+		addTestStudentToDatabase("2", "test2");
+		assertThat(studentRepository.findById("2"))
+			.isEqualTo(new Student("2", "test2"));
+	}
 
 	private void addTestStudentToDatabase(String id, String name) {
 		studentCollection.insertOne(
