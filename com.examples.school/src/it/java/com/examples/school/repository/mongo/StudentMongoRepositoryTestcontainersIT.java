647e2f34e7a89812a38ff50354579ea94e3a92c3
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-12 09:39:22 +0200

    Refactor

--- com.examples.school/src/it/java/com/examples/school/repository/mongo/StudentMongoRepositoryTestcontainersIT.java
+++ com.examples.school/src/it/java/com/examples/school/repository/mongo/StudentMongoRepositoryTestcontainersIT.java
@@ -4,6 +4,8 @@ import static com.examples.school.repository.mongo.StudentMongoRepository.SCHOOL
 import static com.examples.school.repository.mongo.StudentMongoRepository.STUDENT_COLLECTION_NAME;
 
 import org.bson.Document;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.testcontainers.containers.MongoDBContainer;
@@ -17,17 +19,28 @@ public class StudentMongoRepositoryTestcontainersIT {
 
 	@ClassRule
 	public static final MongoDBContainer mongo = new MongoDBContainer("mongo:4.4.3");
-
-	@Test
-	public void test() {
-		MongoClient client = new MongoClient(
+	private MongoClient client;
+	private StudentMongoRepository studentRepository;
+	private MongoCollection<Document> studentCollection;
+	
+	@Before
+	public void setup() {		
+		client = new MongoClient(
 				new ServerAddress(
 						mongo.getContainerIpAddress(),
 						mongo.getMappedPort(27017)));
-		StudentMongoRepository studentRepository = new StudentMongoRepository(client);
+		studentRepository = new StudentMongoRepository(client);
 		MongoDatabase database = client.getDatabase(SCHOOL_DB_NAME);
 		database.drop();
-		MongoCollection<Document> studentCollection = database.getCollection(STUDENT_COLLECTION_NAME);
-		client.close();
+		studentCollection = database.getCollection(STUDENT_COLLECTION_NAME);
+	}
+	
+	@After
+	public void tearDown() {
+		client.close();		
+	}
+
+	@Test
+	public void test() {
 	}
 }
