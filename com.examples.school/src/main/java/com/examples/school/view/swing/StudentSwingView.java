2f981d61fa0fed5524c5455097996ace7ecac814
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-13 15:58:40 +0200

    Implemented first controls initial state

--- com.examples.school/src/main/java/com/examples/school/view/swing/StudentSwingView.java
+++ com.examples.school/src/main/java/com/examples/school/view/swing/StudentSwingView.java
@@ -10,6 +10,11 @@ import javax.swing.border.EmptyBorder;
 
 import com.examples.school.model.Student;
 import com.examples.school.view.StudentView;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import javax.swing.JTextField;
+import java.awt.Insets;
 
 public class StudentSwingView extends JFrame implements StudentView {
 
@@ -19,6 +24,7 @@ public class StudentSwingView extends JFrame implements StudentView {
 	private static final long serialVersionUID = 1L;
 	
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -45,8 +51,30 @@ public class StudentSwingView extends JFrame implements StudentView {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel lblId = new JLabel("id");
+		GridBagConstraints gbc_lblId = new GridBagConstraints();
+		gbc_lblId.insets = new Insets(0, 0, 0, 5);
+		gbc_lblId.anchor = GridBagConstraints.EAST;
+		gbc_lblId.gridx = 0;
+		gbc_lblId.gridy = 0;
+		contentPane.add(lblId, gbc_lblId);
+		
+		textField = new JTextField();
+		textField.setName("idTextBox");
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 0;
+		contentPane.add(textField, gbc_textField);
+		textField.setColumns(10);
 	}
 
 	@Override
