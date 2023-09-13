527619dc50b4ef7f8bdbf87de910a96940936c52
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-13 15:32:41 +0200

    StudentSwingView created

---
+++ com.examples.school/src/main/java/com/examples/school/view/swing/StudentSwingView.java
@@ -0,0 +1,45 @@
+package com.examples.school.view.swing;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import com.examples.school.view.StudentView;
+
+public class StudentSwingView extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					StudentSwingView frame = new StudentSwingView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public StudentSwingView() {
+		setTitle("Student View");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
