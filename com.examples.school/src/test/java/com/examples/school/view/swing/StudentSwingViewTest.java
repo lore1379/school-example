1a11e775926e422250cf54705b6762206fbceda0
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-13 17:35:44 +0200

    Test add disabled when values only contain spaces

--- com.examples.school/src/test/java/com/examples/school/view/swing/StudentSwingViewTest.java
+++ com.examples.school/src/test/java/com/examples/school/view/swing/StudentSwingViewTest.java
@@ -5,6 +5,7 @@ import org.assertj.swing.core.matcher.JButtonMatcher;
 import org.assertj.swing.core.matcher.JLabelMatcher;
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.fixture.JTextComponentFixture;
 import org.assertj.swing.junit.runner.GUITestRunner;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
 import org.junit.Test;
@@ -43,6 +44,23 @@ public class StudentSwingViewTest extends AssertJSwingJUnitTestCase {
 		window.textBox("idTextBox").enterText("1");
 		window.textBox("nameTextBox").enterText("test");
 		window.button(JButtonMatcher.withText("Add")).requireEnabled();
+	}
+	
+	@Test
+	public void testWhenEitherIdOrNameAreBlankThenAddButtonShouldBeDisabled() {
+		JTextComponentFixture idTextBox = window.textBox("idTextBox");
+		JTextComponentFixture nameTextBox = window.textBox("nameTextBox");
+
+		idTextBox.enterText("1");
+		nameTextBox.enterText(" ");
+		window.button(JButtonMatcher.withText("Add")).requireDisabled();
+
+		idTextBox.setText("");
+		nameTextBox.setText("");
+
+		idTextBox.enterText(" ");
+		nameTextBox.enterText("test");
+		window.button(JButtonMatcher.withText("Add")).requireDisabled();
 	}
 
 }
