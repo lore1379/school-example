b645cca4b702caddf3203ef76ee7505ea81d0829
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-14 11:41:46 +0200

    Test allStudents

--- com.examples.school/src/it/java/com/examples/school/view/swing/StudentSwingIT.java
+++ com.examples.school/src/it/java/com/examples/school/view/swing/StudentSwingIT.java
@@ -1,5 +1,7 @@
 package com.examples.school.view.swing;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import org.assertj.swing.annotation.GUITest;
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
 import org.assertj.swing.junit.runner.GUITestRunner;
@@ -42,8 +44,18 @@ public class StudentSwingIT extends AssertJSwingJUnitTestCase {
 		window.show();
 	}
 
-	@Test
-	public void test() {
-
+	@Test @GUITest
+	public void testAllStudents() {
+		// use the repository to add students to the database
+		Student student1 = new Student("1", "test1");
+		Student student2 = new Student("2", "test2");
+		studentRepository.save(student1);
+		studentRepository.save(student2);
+		// use the controller's allStudents
+		GuiActionRunner.execute(
+			() -> schoolController.allStudents());
+		// and verify that the view's list is populated
+		assertThat(window.list().contents())
+			.containsExactly(student1.toString(), student2.toString());
 	}
 }
