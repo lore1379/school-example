3b98dd0c91a8ee8eb82d894cd27297f0f8e462bc
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-14 11:36:57 +0200

    StudentSwingIT setup

---
+++ com.examples.school/src/it/java/com/examples/school/view/swing/StudentSwingIT.java
@@ -0,0 +1,49 @@
+package com.examples.school.view.swing;
+
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.runner.GUITestRunner;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.examples.school.controller.SchoolController;
+import com.examples.school.model.Student;
+import com.examples.school.repository.mongo.StudentMongoRepository;
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+
+@RunWith(GUITestRunner.class)
+public class StudentSwingIT extends AssertJSwingJUnitTestCase {
+	
+	private static int mongoPort =
+			Integer.parseInt(System.getProperty("mongo.port", "27017"));
+	
+	private StudentSwingView studentSwingView;
+	private SchoolController schoolController;
+	private FrameFixture window;
+	private StudentMongoRepository studentRepository;
+
+	@Override
+	protected void onSetUp() throws Exception {
+		studentRepository = new StudentMongoRepository(
+				new MongoClient(
+						new ServerAddress("localhost", mongoPort)));
+		for (Student student : studentRepository.findAll()) {
+			studentRepository.delete(student.getId());
+		}
+		GuiActionRunner.execute(() -> {
+			studentSwingView = new StudentSwingView();
+			schoolController = new SchoolController(studentSwingView, studentRepository);
+			studentSwingView.setSchoolController(schoolController);
+			return studentSwingView;
+		});
+		window = new FrameFixture(robot(), studentSwingView);
+		window.show();
+	}
+
+	@Test
+	public void test() {
+
+	}
+}
