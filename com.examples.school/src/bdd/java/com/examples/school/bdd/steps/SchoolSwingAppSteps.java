e0c711ac0730a6c640af3e31d6a9f0e2fa98a3d1
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-15 11:44:34 +0200

    Refactor

--- com.examples.school/src/bdd/java/com/examples/school/bdd/steps/SchoolSwingAppSteps.java
+++ com.examples.school/src/bdd/java/com/examples/school/bdd/steps/SchoolSwingAppSteps.java
@@ -4,25 +4,40 @@ import org.bson.Document;
 
 import com.mongodb.MongoClient;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class SchoolSwingAppSteps {
 
+	private static final String COLLECTION_NAME = "test-collection";
+	private static final String DB_NAME = "test-db";
+	
+	private MongoClient mongoClient;
+
+	@Before
+	public void setup() {	
+		mongoClient = new MongoClient();
+		mongoClient.getDatabase(DB_NAME).drop();
+	}
+	
+	@After
+	public void tearDown() {
+		mongoClient.close();
+	}
+	
 	@Given("The database contains a student with id {string} and name {string}")
 	public void the_database_contains_a_student_with_id_and_name(
 			String id, String name) {
-	    MongoClient mongoClient = new MongoClient();
-	    mongoClient.getDatabase("test-db").drop();
 	    mongoClient
-	    	.getDatabase("test-db")
-	    	.getCollection("test-collection")
+	    	.getDatabase(DB_NAME)
+	    	.getCollection(COLLECTION_NAME)
 	    	.insertOne(
 	    			new Document()
 	    				.append("id", id)
 	    				.append("name", name));
-	    mongoClient.close();
 	}
 
 	@When("The Student View is shown")
