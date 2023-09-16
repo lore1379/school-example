fdae3a5a927d5f8b8f4b82248ba6bdd44cebdd39
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-16 09:59:24 +0200

    Separate step files

--- com.examples.school/src/bdd/java/com/examples/school/bdd/steps/SchoolSwingAppSteps.java
+++ com.examples.school/src/bdd/java/com/examples/school/bdd/steps/SchoolSwingAppSteps.java
@@ -13,55 +13,27 @@ import org.assertj.swing.core.GenericTypeMatcher;
 import org.assertj.swing.core.matcher.JButtonMatcher;
 import org.assertj.swing.finder.WindowFinder;
 import org.assertj.swing.fixture.FrameFixture;
-import org.bson.Document;
-
-import com.mongodb.MongoClient;
-import com.mongodb.ServerAddress;
 
 import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class SchoolSwingAppSteps {
-
-	private static final String COLLECTION_NAME = "test-collection";
-	private static final String DB_NAME = "test-db";
+	
+	static final String DB_NAME = "test-db";
+	static final String COLLECTION_NAME = "test-collection";
 	
 	private static int mongoPort =
 			Integer.parseInt(System.getProperty("mongo.port", "27017"));
 	
-	private MongoClient mongoClient;
 	private FrameFixture window;
-
-	@Before
-	public void setup() {	
-		mongoClient = new MongoClient(
-				new ServerAddress("localhost", mongoPort));
-		mongoClient.getDatabase(DB_NAME).drop();
-	}
 	
 	@After
 	public void tearDown() {
-		mongoClient.close();
 		if (window != null)
 			window.cleanUp();
 	}
 	
-	@Given("The database contains the students with the following values")
-	public void the_database_contains_the_students_with_the_following_values(
-			List<List<String>> values) {
-		values.forEach(
-			v -> mongoClient
-				.getDatabase(DB_NAME)
-				.getCollection(COLLECTION_NAME)
-				.insertOne(
-					new Document()
-						.append("id", v.get(0))
-						.append("name", v.get(1)))
-		);
-	}
 
 	@When("The Student View is shown")
 	public void the_Student_View_is_shown() {
